94acdf47d1ec6e0ba1d78b3f31f347ce0b6e7e54
Ao Li <leeleo3x@gmail.com>
2025-02-25 15:05:29 +0000

    update.

---
+++ scripts/repro.py
@@ -0,0 +1,32 @@
+from multiprocessing import Pool
+import os
+
+CONFIGURATIONS = [
+    "ant",
+    "closure",
+    "maven",
+    "rhino",
+    "chocopy",
+    "gson",
+    "jackson"
+
+]
+ALGOS = [
+    "ei",
+    "zest",
+    "zeugma-linked",
+    "bedivfuzz-simple",
+    "bedivfuzz-structure",
+]
+
+
+def get_commands():
+
+    for config in CONFIGURATIONS:
+        for algo in ALGOS:
+            for iter in range(0, 8):
+                command = f"mvn -pl :zeugma-evaluation-tools meringue:analyze -P{config},{algo} -Dmeringue.outputDirectory=/data/aoli/havoc_eval/cov-test/{config}-{algo}-results-{iter}"
+                yield command
+
+with Pool(10) as p:
+    p.map(os.system, get_commands())
